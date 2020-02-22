7d7cc6c0078d8ddb195c11dd80d8a6f367a86f69
Jay Vigilla <JayRVigilla@gmail.com>
2020-02-21 20:56:20 -0800

    redoing functions after re-watching tutorial through FreeCodeingCamp.

--- MemoryCards/script.js
+++ MemoryCards/script.js
@@ -1,46 +1,65 @@
+
 // init listeners
   // wait for DOM to load
   // suppress reload clears everything - event.preventDefault() when hitting 'new game'
+var first, second;
+    cards = document.querySelectorAll('.card'), 
+    facesF = document.querySelectorAll('.front'),
+    facesB = document.querySelectorAll('.back');
 window.onload = function(){
 
-var ext = 'my-icons-collection/png/',  
-
-    deck = [ext+'001-cleaver.png', ext+'002-chef.png', ext+'003-spatula.png', ext+'004-oven.png', ext+'006-cook.png', 
-    ext+'007-apron.png', ext+'011-cold.png', ext+'012-kitchen-1.png', ext+'001-cleaver.png', ext+'002-chef.png', ext+'003-spatula.png', 
-    ext+'004-oven.png', ext+'006-cook.png', ext+'007-apron.png', ext+'011-cold.png', ext+'012-kitchen-1.png'];
-
-function shuffleNewGame() {
-    // function to shuffle deck and 'deal'/itterate face values into class=front
-    for (let i = deck.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-          [deck[i], deck[j]] = [deck[j], deck[i]];
-        }
-    var facesF = document.querySelectorAll(".front"),
-        facesB = document.querySelectorAll(".back");
-        facesF.forEach((a,i) =>{
-          a.src = deck[i];
-        });
-        // if class=back then back is visible
-        facesB.forEach(a => {
-          a.style.opacity = "1";
-        });
-    // flipped === undefined for all 'cards'/divs
+  // hover over card -> if class=back && flipped=false then shrink to 95%
+  facesB.forEach(a => a.addEventListener('mouseover', function(){
+    event.target.style.transform = 'scale(.95)';
     }
+  ));
 
-function select(){
-  // hover over card -> if class=back && flipped=false then shrink to 85%
-  document.getElementsByClassName('back').style.
-}
+  facesB.forEach(a => a.addEventListener('mouseout', function(){
+    event.target.style.transform = 'scale(1)';
+    }
+  ));
+
+// onclick -> if class!==matched THEN class=back goes transparent, class=front goes visible, hand.push(frontImgFileName)
+facesB.forEach(a=> a.addEventListener("click", flip));
+
+
+
+
+  function flip(){
+    if (event.target.parentElement.className !== 'flipped'){
+      event.target.style.opacity = '0';
+      event.target.parentElement.classList.add('flipped');
+    };
+    if(first === undefined){
+      first = event.target.parentElement;
+      alert('first assigned');
+    }else{
+      second = event.target.parentElement;
+      alert('second assigned');
+      if(first.dataset.framework === second.dataset.framework){
+        // remove card class, clear first and second
+        first.classList.remove('card');
+        second.classList.remove('card');
+        alert("got there");
+      }else{
+        faceDown();
+      }
+      first = undefined;
+      second = undefined;
+    }
+  }
+
+  function faceDown(){
+    let flipt = document.querySelectorAll('.flipped');
+    
+    // **2-20 problem** not trigering alert when mismatched cards nor making the back class opaque. 
+    flipt.forEach(function(x){
+      if(x.classList.contains('cards')){
+        alert('black it out');
+        x.childNodes[1].style.opacity = '1'
+      }
+      });
+  }  
+
   
-
-// flip()
-    // onclick -> if class!==matched THEN class=back goes transparent, class=front goes visible, hand.push(frontImgFileName)
-
-//  check() - checks cards for match
-    // var hand = []
-    // IF hand.length==2 && hand.every()===false THEN all cards where class!==mathced -> back=visible, hand=[]
-    // IF hand.length==2 && hand.every()===true THEN add class MATCHED to the targets in hand
-    shuffleNewGame();
-
-
   }
