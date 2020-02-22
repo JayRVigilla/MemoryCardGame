e41a1ac6c5179b6a5d8f8a8fbec4af3c48d1f1aa
Jay Vigilla <JayRVigilla@gmail.com>
2020-02-21 22:19:23 -0800

    nearly done! fix shuffle and match check. match doesn't work since moving cards into individual divs instead of four card divs inside of four row divs

--- MemoryCards/script.js
+++ MemoryCards/script.js
@@ -5,8 +5,13 @@
 var first, second;
     cards = document.querySelectorAll('.card'), 
     facesF = document.querySelectorAll('.front'),
-    facesB = document.querySelectorAll('.back');
+    facesB = document.querySelectorAll('.back'),
+    flipt = document.querySelectorAll('.flipped');
+
+
 window.onload = function(){
+
+  shuffle();
 
   // hover over card -> if class=back && flipped=false then shrink to 95%
   facesB.forEach(a => a.addEventListener('mouseover', function(){
@@ -20,46 +25,65 @@ window.onload = function(){
   ));
 
 // onclick -> if class!==matched THEN class=back goes transparent, class=front goes visible, hand.push(frontImgFileName)
-facesB.forEach(a=> a.addEventListener("click", flip));
+  facesB.forEach(a=> a.addEventListener("click", flip));
 
+// New Game Button shuffles new game
+// ** PROBLEM BUTTON DOESN'T RESTART AND SHUFFLE GAME**
+document.addEventListener(document.getElementById('newgame'), shuffle);
 
-
-
+// ** PROBLEM ** Match test not working since moving cards out of 4 div rows with 4 div cards inside
   function flip(){
     if (event.target.parentElement.className !== 'flipped'){
       event.target.style.opacity = '0';
       event.target.parentElement.classList.add('flipped');
     };
     if(first === undefined){
-      first = event.target.parentElement;
-      alert('first assigned');
+      first = event.target;
     }else{
-      second = event.target.parentElement;
-      alert('second assigned');
+      if (event.target === first){
+        return;
+      }
+      second = event.target;
+      // MATCH!!!
       if(first.dataset.framework === second.dataset.framework){
         // remove card class, clear first and second
         first.classList.remove('card');
         second.classList.remove('card');
-        alert("got there");
+        // ** all matched test not triggering **
+        // if(flipt.length === 16){
+        //   alert("Congratulations! You've matched them all!!")
+        //   } 
+      // Mismatch :( flip cards back over     
       }else{
         faceDown();
       }
       first = undefined;
       second = undefined;
     }
+    if(cards.length === 0){
+    alert("Congratulations! You've matched them all!!")
+    }  
   }
 
   function faceDown(){
-    let flipt = document.querySelectorAll('.flipped');
-    
-    // **2-20 problem** not trigering alert when mismatched cards nor making the back class opaque. 
     flipt.forEach(function(x){
-      if(x.classList.contains('cards')){
-        alert('black it out');
-        x.childNodes[1].style.opacity = '1'
+      if(x.classList.contains('card')){
+        alert('boobs');
+        // add timer so that player can see the second card before flipping over
+        x.childNodes[1].style.opacity = '1';
       }
       });
   }  
 
+  // Shuffle function assigns flexbox order based on Math.random * cards.length;
+  function shuffle(){
+    cards.forEach(card=>{
+      let randomPos = Math.floor(Math.random() * 16);
+      card.style.order = randomPos;
+    }
+      )
+  }
+  // score keeper?
+  // timer under condition to start on shuffle and end when cards.length === 0
   
   }
